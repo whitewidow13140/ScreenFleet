4f4b13c4b1efb77f52e40e19325d2a3a3a439899
Alex <alexis.doumax@gmail.com>
2018-05-16 15:14:39 +0200

    added Update and Delete

--- main.go
+++ main.go
@@ -53,6 +53,25 @@ func ConnectToDB() {
 	}
 }
 
+func (s *server) showTable() {
+	fmt.Println("# Querying")
+	rows, err := s.db.Query("SELECT * FROM television")
+	checkErr(err)
+
+	fmt.Printf("%20v | %15v | %15v | %10v | %15v \n\n", "id", "name", "ip", "status", "compositionID")
+	for rows.Next() {
+		var id string
+		var name string
+		var ip string
+		var status int
+		var compositionID string
+		err = rows.Scan(&id, &name, &ip, &status, &compositionID)
+		checkErr(err)
+		fmt.Printf("%20v | %15v | %15v | %10v | %15v\n", id, name, ip, status, compositionID)
+	}
+
+}
+
 func checkErr(err error) {
 	if err != nil {
 		panic(err)
@@ -96,19 +115,39 @@ func (s *server) ListTelevisions(ctx context.Context, req *television.ListTelevi
 	}
 	err = rows.Err()
 
-	//	if err := rows.Scan(&tv.ID, &tv.Name, &tv.IP, &tv.Status, &tv.CompositionID); err != nil {
 	defer rows.Close()
 
 	return &television.ListTelevisionsResp{Televisions: tvs}, nil
 }
 
-func (s *server) UpdateTelevision(ctx context.Context) (*television.Television, error) {
+func (s *server) UpdateTelevision(ctx context.Context, req *television.Television) (*television.Television, error) {
 	//Doit update television.status ===> Online/Offline
-	return &television.Television{}, nil
+	_, err := s.db.ExecContext(ctx, "update television set id=$1, name=$2, ip=$3, status=$4, composition_id=$5 where id=$6",
+		req.ID, req.Name, req.IP, req.Status, req.CompositionID, req.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tv := television.Television{}
+	row := s.db.QueryRowContext(ctx, "SELECT id, name, ip, status, composition_id FROM television WHERE id=$1", req.ID)
+
+	if err := row.Scan(&tv.ID, &tv.Name, &tv.IP, &tv.Status, &tv.CompositionID); err != nil {
+		return nil, grpc.Errorf(codes.Internal, "could not scan %v", err)
+	}
+
+	return &tv, nil
 }
 
-func (s *server) DeleteTelevision(ctx context.Context, req television.DeleteTelevisionReq) empty.Empty {
-	return empty.Empty{}
+func (s *server) DeleteTelevision(ctx context.Context, req *television.DeleteTelevisionReq) (*empty.Empty, error) {
+	_, err := s.db.ExecContext(ctx, "delete from television where id=$1", req.Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if err := row.Scan(&tv.ID, &tv.Name, &tv.IP, &tv.Status, &tv.CompositionID); err != nil {
+	// 	return nil, grpc.Errorf(codes.Internal, "could not scan %v", err)
+	// }
+
+	return &empty.Empty{}, nil
 }
 
 func main() {
@@ -136,7 +175,8 @@ func main() {
 	//// TESTS ////////////
 	///////////////////////
 
-	// ConnectToDB()
+	// showTable()
+	srv.showTable()
 	fmt.Printf("\n")
 
 	// GetTelevisions()
@@ -153,6 +193,25 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(resp)
+	fmt.Printf("\n")
+
+	// DeleteTevision()
+	_, err = srv.DeleteTelevision(context.Background(), &television.DeleteTelevisionReq{Id: "00000000002"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\n")
+
+	// UpdateTelevision()
+	_, err = srv.UpdateTelevision(context.Background(),
+		&television.Television{ID: "00000000001", Name: "Bite", IP: "192.168.0.1", Status: 4, CompositionID: "5"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\n")
+
+	// showTable()
+	srv.showTable()
 	fmt.Printf("\n")
 
 	if err := s.Serve(lis); err != nil {
